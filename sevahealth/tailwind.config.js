bffa662ed70fd4e11b09300335f5498d78edd141
anshjw <anshjw1926@gmail.com>
2026-01-18 03:03:21 +0530

    THeme Changer added

---
+++ sevahealth/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/app/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}",
+    "./src/context/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
