184d105e048d8995c659aa7a00c17e0817422f5f
anshjw <anshjw1926@gmail.com>
2026-01-18 05:50:33 +0530

    Registration and login page fixed

--- sevahealth/src/app/dashboard/patients/page.js
+++ sevahealth/src/app/dashboard/patients/page.js
@@ -14,7 +14,7 @@ import {
   User,
   Activity,
 } from "lucide-react";
-import AddPatientModal from "@/src/app/dashboard/patients/AddPatientModal";
+import AddPatientModal from "@/src/app/dashboard/patients/AddpatientModal";
 import PatientDetails from "@/src/app/dashboard/patients/PatientDetails";
 
 // --- ROBUST MOCK DATA ---
